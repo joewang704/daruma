a0eee89943db96c02eb678ea9c7d579dfac28178
Douglas Cheong <dcheong@gatech.edu>
2016-09-10 21:33:07 -0400

    moved some inline style into variables uguuuuuu

--- app/src/components/DayColumn/DayColumn.js
+++ app/src/components/DayColumn/DayColumn.js
@@ -3,31 +3,34 @@ import { dayMomentToStr } from '../../utils/calendar.js'
 import Item from '../Item'
 
 const DayColumn = ({ dayMoment, items, createEmptyItem }) => {
+  const wrapperStyle = {
+    height: '100%',
+    width: `${100/7}%`,
+    borderRight: '1px solid #ddd',
+  }
+  const labelStyle = {
+    width: '100%',
+    height: '10%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+  const mainStyle = {
+    width: '100%',
+    height: '90%',
+    backgroundColor: 'white',
+    cursor: 'pointer',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
   return (
-    <div style={{
-      height: '100%',
-      width: `${100/7}%`,
-      borderRight: '1px solid #ddd',
-    }}>
-      <div style={{
-        width: '100%',
-        height: '10%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-        { dayMomentToStr(dayMoment) }
+    <div style={wrapperStyle}>
+      <div style={labelStyle}>
+        {dayMomentToStr(dayMoment)}
       </div>
       <div
-        style={{
-          width: '100%',
-          height: '90%',
-          backgroundColor: 'white',
-          cursor: 'pointer',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
+        style={mainStyle}
         onClick={() => createEmptyItem(dayMoment)}
       >
         {
