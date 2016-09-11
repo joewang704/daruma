697e1a0b608502930b5cc0834865a0070f6a287a
Douglas Cheong <dcheong@gatech.edu>
2016-09-10 21:49:28 -0400

    Added a daruma

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import WeekView from './WeekView'
+import Daruma from './Daruma'
 
 const App = () => {
   return (
     <div id="app">
       <WeekView />
+      <Daruma />
     </div>
   )
 }
---
+++ app/src/components/Daruma.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Daruma = () => {
+  const mainStyle = {
+    position: 'absolute',
+    right: '5%',
+    bottom: '10%',
+    height: '10%',
+  }
+  return (
+    <img style={mainStyle} src="/static/img/daruma.png" />
+  )
+}
+
+export default Daruma
